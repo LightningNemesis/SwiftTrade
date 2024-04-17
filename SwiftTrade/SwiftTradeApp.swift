aea3e83b4803e856b22f6689715973790b231cd6
LightningNemesis <ak.kumar220919@gmail.com>
2024-04-16 17:46:29 -0700

    Initial Commit

---
+++ SwiftTrade/SwiftTradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTradeApp.swift
+//  SwiftTrade
+//
+//  Created by Abhishek Kumar on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
